2a215074d6b5e06b9fb0a772ae9e125f280cb488
abdullah shaikh <abdullahsjnshaikh@gmail.com>
2025-11-28 23:25:20 -0500

    Add SingleBeforeAfterGallery to shop pages

--- app/products/zumfali-hair-oil-nourishing-30ml-10ml-bundle/page.tsx
+++ app/products/zumfali-hair-oil-nourishing-30ml-10ml-bundle/page.tsx
@@ -9,6 +9,7 @@ import ProductFunnel from '@/components/ProductFunnel';
 import ProductFAQ from '@/components/ProductFAQ';
 import ProblemSolution from '@/components/ProblemSolution';
 import ReviewGrid from '@/components/ReviewGrid';
+import SingleBeforeAfterGallery from '@/components/SingleBeforeAfterGallery';
 import {
   isShopifyLoggedIn,
   redirectToShopifySignup,
@@ -459,6 +460,7 @@ export default function ProductPage() {
           </div>
         </div>
         
+        <SingleBeforeAfterGallery />
         <ProblemSolution />
         <ProductFunnel />
         <ReviewGrid />
